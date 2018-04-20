a5901f1315467a7d2197c0590fb6484229a36800
Lindsay Mulhollen <lindsaymulhollen@lindsays-mbp.attlocal.net>
2018-04-19 23:37:44 -0500

    added social icons to about

--- src/About.js
+++ src/About.js
@@ -24,6 +24,13 @@ class About extends Component {
                         <br />                        
                         I'm excited to bring my unique background to the development world where I plan to make my mark with innovative thinking and motivation to tackle even the most unthinkable projects.
                     </p>
+                    <div>
+                        <div class="">
+                            <a href="https://www.linkedin.com/in/mulhollen/" target="_blank" className="mr-3"><i class="fab fa-linkedin-in fa-2x"></i></a>
+                            <a href="https://github.com/mulhollen" target="_blank" className="mr-3"><i class="fab fa-github fa-2x"></i></a>
+                            <a href="https://www.instagram.com/didicrytoday" target="_blank"><i class="fab fa-instagram fa-2x"></i></a>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
