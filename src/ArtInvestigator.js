e85cfbfffc3031bb3e367401a874cc0f7cd22db2
Lindsay Mulhollen <lindsaymulhollen@lindsays-mbp.attlocal.net>
2018-04-27 18:10:03 -0500

    project page

--- src/ArtInvestigator.js
+++ src/ArtInvestigator.js
@@ -13,7 +13,18 @@ class ArtInvestigator extends Component {
     render() {
         return (
             <div>
-                <h1>Hey gurl Hey</h1>
+                <Video className="static" video="https://assets.bonappetit.com/clips/5ace10e2d2b1c178c2958818/720p/pass/Kimchi_Pancakes-Step_01_v1_20180314.mp4.mp4" />
+                <div className="d-flex justify-content-center flex-column port-h1 bodyWidth">
+                    <h1 className="m-3 text-center">What do you call an aligator wearing a vest?</h1>
+                    <h3 className="m-3">Learn about art + grow emotional intelligence the fun way. Art Investigator is an accompaninment app for The Frist Museum's <i>Feat.</i> exhibit by Nick Cave. Focusing on building emotional intelligence, interest in art, and creating points of interaction for children ages 7 to 11 and their grownup.</h3>
+                </div>
+                <div className="bodyWidth yellow d-flex flex-row my-5">
+                    <img className="w-50" src={lindsay} alt="" />
+                    <div>
+                        <h3>the challenge:</h3>
+                        <p></p>
+                    </div>
+                </div>
             </div>
         );
     }
