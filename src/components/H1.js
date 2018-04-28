58b8a9c686418c67b4e0cc008b8d4cfe05073e0f
Lindsay Mulhollen <lindsaymulhollen@lindsays-mbp.attlocal.net>
2018-04-27 19:09:16 -0500

    main h1 styled up a bit

--- src/components/H1.js
+++ src/components/H1.js
@@ -6,18 +6,20 @@ import '../App.css';
 class H1 extends Component {
     render() {
         return (
-            <div className="m-5 pt-5 bodyWidth mainH1 d-flex justify-content-center flex-column">
-                <h1 className="text-center">{emojis[775]}&nbsp;Hey, it's Lindsay + I Make</h1>
-                <div className="ml-5 pb-5">
-                    {/* <h1 className="display-4">+ I Make&nbsp;</h1> */}
-                    <h1 className="animated-words">
-                        <span>Fun&nbsp;Websites.</span>
-                        <span>Thoughtful&nbsp;&nbsp;UI&nbsp;Design.</span>
-                        <span>Inovative&nbsp;&nbsp;User&nbsp;Experiences.</span>
-                        <span>An&nbsp;Awesome&nbsp;Dance&nbsp;Party.</span>
-                        <span>Decent&nbsp;Guacamole.</span>
-                        <span>Coding&nbsp;Fun.</span>
-                    </h1>
+            <div className="m-5 pt-5 mainH1 d-flex justify-content-center flex-column">
+                <h1 className="ml-words yellow">{emojis[775]}&nbsp;Hey, it's Lindsay!</h1>
+                <div className="d-flex flex-row justify-content-start ml-words">
+                    <h1 className="yellow">I Make:</h1>
+                    <div className="">
+                        <h1 className="animated-words">
+                            <span className="yellow">Fun&nbsp;Websites.</span>
+                            <span className="yellow">Thoughtful&nbsp;UI&nbsp;Design.</span>
+                            <span className="yellow">Inovative&nbsp;User&nbsp;Experiences.</span>
+                            <span className="yellow">An&nbsp;Awesome&nbsp;Dance&nbsp;Party.</span>
+                            <span className="yellow">Decent&nbsp;Guacamole.</span>
+                            <span className="yellow">Coding&nbsp;Fun.</span>
+                        </h1>
+                    </div>
                 </div>
             </div>
         );
