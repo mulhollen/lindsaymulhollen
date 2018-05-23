64328104d7ae982fd79a4ff4659df2a20c22c382
Lindsay Mulhollen <lindsaymulhollen@lindsays-mbp.attlocal.net>
2018-05-22 21:14:36 -0500

    hero video working

--- src/components/H1.js
+++ src/components/H1.js
@@ -7,21 +7,21 @@ class H1 extends Component {
     render() {
         return (
             <div className="margin-neg m-5 pt-5 mainH1 d-flex justify-content-center flex-column">
-                <h1 className="ml-words yellow">{emojis[775]}&nbsp;Hey, it's Lindsay!</h1>
+                <h1 className="ml-words display-4">{emojis[775]}&nbsp;hey, it's lindsay!</h1>
                 <div className="d-flex flex-row justify-content-start ml-words">
-                    <h1 className="yellow">I Make:</h1>
+                    <h1 className="display-4">i make:</h1>
                     <div className="">
-                        <h1 className="animated-words">
-                            <span className="yellow">Fun&nbsp;Websites.</span>
-                            <span className="yellow">Thoughtful&nbsp;UI&nbsp;Design.</span>
-                            <span className="yellow">Inovative&nbsp;User&nbsp;Experiences.</span>
-                            <span className="yellow">An&nbsp;Awesome&nbsp;Dance&nbsp;Party.</span>
-                            <span className="yellow">Decent&nbsp;Guacamole.</span>
-                            <span className="yellow">Coding&nbsp;Fun.</span>
+                        <h1 className="animated-words display-4">
+                            <span className="">fun&nbsp;websites.</span>
+                            <span className="">thoughtful&nbsp;UI</span>
+                            <span className="">innovative&nbsp;UX</span>
+                            <span className="">an&nbsp;awesome<br />dance&nbsp;party.</span>
+                            <span className="">decent<br />guacamole.</span>
+                            <span className="">coding&nbsp;fun.</span>
                         </h1>
                     </div>
                 </div>
-                <h1 className="ml-words yellow">&nbsp;</h1>                
+                <h1 className="ml-words display-4">&nbsp;</h1>                
             </div>
         );
     }
--- src/components/Video.js
+++ src/components/Video.js
@@ -4,7 +4,7 @@ import '../App.css';
 
 function Video(props){ 
         return (
-            <video loop webkit-playsInline="true" playsInline="playsInline" autoplay="autoplay" className="w-100">
+            <video id="myvid" loop autoplay="autoplay" className="w-100">
                 <source src={props.video} type="video/mp4" />
             </video>
         );
